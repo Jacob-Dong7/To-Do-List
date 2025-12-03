89420f865669677f6e4697a7f54edbe1c307e59d
JDOT <JDON219206@gmail.com>
2025-12-03 21:07:11 +1100

    Feat: Add initial implementation of the To-Do List application with main window and UI setup

---
+++ mainwindow.cpp
@@ -0,0 +1,8 @@
+#include <QMainWindow>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
